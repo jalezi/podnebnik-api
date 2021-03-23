2a9b39a61a9dd87d3208a50b4abc9639960a3627
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-23 14:55:04 +0100

    test(fetchData): add tests: csvToJSON, getJSON

--- src/tests/fetchData.test.js
+++ src/tests/fetchData.test.js
@@ -1,14 +1,78 @@
 import { csvToJSON, getJSON } from '../fetchData';
+import { DataError } from '../Errors/ApplicationError';
 
 describe('fetchData', () => {
   describe('csvToJSON', () => {
-    it('should be a function', () => {
+    it('should be a function - csvToJSON', () => {
       expect(csvToJSON).toBeInstanceOf(Function);
+    });
+    it('should return empty array', () => {
+      const result = csvToJSON();
+      expect(result).toBeInstanceOf(Array);
+      expect(result).toHaveLength(0);
+    });
+    it('should throw DataError', () => {
+      expect(() => csvToJSON('x')).toThrowError(DataError);
+    });
+    it('should return array with error obj', () => {
+      const result1 = csvToJSON([['value1']], ['key1', 'key2']);
+      expect(result1).toBeInstanceOf(Array);
+      expect(result1).toHaveLength(1);
+      expect(result1[0]).toHaveProperty('error');
+      const result2 = csvToJSON([['value1', 'value2']], ['key1']);
+      expect(result2).toBeInstanceOf(Array);
+      expect(result2).toHaveLength(1);
+      expect(result2[0]).toHaveProperty('error');
+    });
+    it('should return correct array with length 1 and obj', () => {
+      const result = csvToJSON([['value1', 'value2']], ['key1', 'key2']);
+      expect(result).toBeInstanceOf(Array);
+      expect(result).toHaveLength(1);
+      expect(result[0]).toEqual({ key1: 'value1', key2: 'value2' });
+    });
+    it('should return correct array with length 2 and obj', () => {
+      const result = csvToJSON(
+        [
+          ['value1', 'value2', 'value3'],
+          ['value1', 'value2', 'value3'],
+        ],
+        ['key1', 'key2.nested1', 'key3.nested2']
+      );
+      expect(result).toBeInstanceOf(Array);
+      expect(result).toHaveLength(2);
+      expect(result[0]).toHaveProperty('key1');
+      expect(result[0].key2).toHaveProperty('nested1');
     });
   });
   describe('getJSON', () => {
-    it('should be a function', () => {
+    it('should be a function - getJSON', () => {
       expect(getJSON).toBeInstanceOf(Function);
+    });
+
+    it('should return obj with key and error name "DataError"', () => {
+      const func = jest.fn(([key, data]) => getJSON({}, [key, data]));
+      const result = func(['myKey', 'header1,header2,header3\nitem1,item2\n']);
+      expect(result).toHaveProperty('myKey');
+      expect(result.myKey).toBeInstanceOf(Array);
+      expect(result.myKey).toHaveLength(1);
+      expect(result.myKey[0]).not.toHaveProperty('header1');
+      expect(result.myKey[0]).toHaveProperty('error');
+      expect(result.myKey[0].error).toHaveProperty('name');
+      expect(result.myKey[0].error.name).toBe('DataError');
+    });
+
+    it('should return obj with key and value', () => {
+      const func = jest.fn(([key, data]) => getJSON({}, [key, data]));
+      const result = func([
+        'myKey',
+        'header1,header2,header3\nitem1,item2,item3\n',
+      ]);
+      expect(result).toHaveProperty('myKey');
+      expect(result.myKey).toBeInstanceOf(Array);
+      expect(result.myKey).toHaveLength(1);
+      expect(Object.keys(result.myKey[0])).toHaveLength(3);
+      expect(result.myKey[0]).toHaveProperty('header1');
+      expect(result.myKey[0].header1).toBe('item1');
     });
   });
 });
