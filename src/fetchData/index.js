2854f314aae6a13fffee0579a782d8a4b0e096a5
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-23 11:59:55 +0100

    refactor(fetchData): validate csvToJSON

--- src/fetchData/index.js
+++ src/fetchData/index.js
@@ -1,44 +1,66 @@
 import _ from 'lodash';
 
+import { DataError } from '../Errors/ApplicationError/index.js';
+
 import { Historical_URL, Projections_URL } from '../dicts/CSV_Url.js';
 import fetchData from './fetchData.js';
 
-const csvToJson = (data, header) =>
-  data.map(line => {
-    const currentLine = line.split(',');
-    const obj = currentLine.reduce((acc, item, i) => {
-      const keys = header[i].split('.');
+const isNotArray = value => !(value instanceof Array);
 
-      if (keys.length === 0 || typeof keys === 'string') {
-        return acc;
-      }
-      if (keys.length === 1) {
-        acc[keys[0]] = item;
-        return acc;
-      }
+const csvToJSON = (data = [], header = []) => {
+  if (isNotArray(data) || isNotArray(header)) {
+    throw new DataError({ data, header }, 'One argument is not an Array!');
+  }
 
-      const newItems = item + '';
-      const newKeys = keys.slice(1, keys.length).join('.');
-      const json = csvToJson([newItems], [newKeys])[0];
-      acc[keys[0]] = _.merge(acc[keys[0]], json);
-      return acc;
-    }, {});
-    return obj;
+  const result = data.map(line => {
+    try {
+      const lineResult = line.reduce((acc, item, i) => {
+        const keys = header[i].split('.');
+        const key = keys[0];
+
+        if (line.length !== header.length) {
+          throw new DataError(
+            { key, data: data.length, header: header.length },
+            'Data length is not equal header length!'
+          );
+        }
+
+        if (keys.length === 1) {
+          acc[key] = item;
+          return acc;
+        }
+        const newHeader = keys.slice(1, keys.length).join('.');
+        const json = csvToJSON([[item]], [newHeader]);
+        acc[key] = _.merge(acc[key], json[0]);
+
+        return acc;
+      }, {});
+      return lineResult;
+    } catch (error) {
+      return [{ msg: error.message, ...error }];
+    }
   });
+  return result;
+};
 
 const mapSortByYearAsc = (a, b) => a.year - b.year;
 
 const getJSON = (acc, [key, data]) => {
-  const lines = data.split('\n');
-  const header = lines[0].split(',');
+  const lines = data.split('\n').map(item => item.split(','));
+  const header = lines[0];
   const dataLines = lines.slice(1, -1);
-  acc[key] = csvToJson(dataLines, header).sort(mapSortByYearAsc);
-  return acc;
+  try {
+    acc[key] = csvToJSON(dataLines, header).sort(mapSortByYearAsc);
+    return acc;
+  } catch (error) {
+    acc[key] = { key, msg: error.message, ...error };
+    return acc;
+  }
 };
 
 const fetchAll = async () => {
   const historicalData = await fetchData(Historical_URL);
-  const historical = historicalData.reduce(getJSON, {});
+  const historical = [...historicalData].reduce(getJSON, {});
 
   const projectionsData = await fetchData(Projections_URL);
   const projections = [...projectionsData].reduce(getJSON, {});
@@ -51,5 +73,7 @@ const fetchAll = async () => {
 };
 
 const data = fetchAll();
+
+export { csvToJSON, getJSON };
 
 export default data;
