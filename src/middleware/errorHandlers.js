229bac7ce23a362f1e1ee981b63e81d7ed120bb9
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-13 11:06:41 +0200

    feat(errorHandler): unknown NODE_ENV res

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -38,5 +38,11 @@ export const errorHandler = (error, _req, res, next) => {
       },
     });
   }
-  next(error);
+
+  res.status(500).json({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error',
+    },
+  });
 };
